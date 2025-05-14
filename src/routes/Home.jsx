496c22a680ad2cbfd55d918127135a543fe96aff
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-05-14 11:17:43 +0200

    setup basic file structure

---
+++ src/routes/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer.jsx"
+import Navigation from "../components/Navigation.jsx"
+import ImageSlider from "../components/ImageSlider.jsx"
+
+export default function Home() {
+  return (
+    <>
+      <Navigation />
+			<ImageSlider />
+      <Footer />
+    </>
+  )
+}
