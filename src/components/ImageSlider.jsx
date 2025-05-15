d831c6ee4e83d37114462a60d5e979051a25c15c
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-05-15 15:07:01 +0200

    creating the slider, start with left side

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,3 +1,48 @@
+import { useState } from "react"
+import { sliderItems } from "../data/sliderItems.js"
+import LinkButton from "./LinkButton.jsx"
+
 export default function ImageSlider() {
-	return <></>
+  const [sliderIndex, setSliderIndex] = useState(0)
+
+  return (
+    <section className="slider-wrapper">
+      <div className="slider-content-container">
+        <div
+          className="slider-content"
+          style={{ translate: `${(sliderIndex % sliderItems.length) * -100}%` }}
+        >
+          {sliderItems.map((item, index) => {
+            return (
+              <div key={index} className="slider-content-item">
+                <h1>{item.title}</h1>
+                <p>{item.text}</p>
+                <LinkButton link={item.link} text={item.linkText}></LinkButton>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className="slider-image">
+        {sliderItems.map((item, index) => {
+          return <img key={index} src={item.imageUrl} alt={item.linkText} />
+        })}
+        <div className="slider-dots-container">
+          {sliderItems.map((item, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => setSliderIndex(index)}
+                className={sliderIndex === index && "active"}
+              ></button>
+            )
+          })}
+        </div>
+        <div className="slider-buttons-container">
+          <button onClick={() => setSliderIndex(sliderIndex - 1)}>-</button>
+          <button onClick={() => setSliderIndex(sliderIndex + 1)}>+</button>
+        </div>
+      </div>
+    </section>
+  )
 }
---
+++ src/data/sliderItems.js
@@ -0,0 +1,30 @@
+export const sliderItems = [
+    {
+      title: "Dein Beat. Dein Sound.",
+      text: "Entdecke eine riesige Auswahl an Becken, Fellen, Sticks und Hardware, um dein Drumset optimal zu ergänzen und deinen individuellen Sound zu perfektionieren. ",
+      link: "/shop",
+      linkText: "Jetzt Zubehör finden",
+      imageUrl: "",
+    },
+    {
+			title: "Goldener Klang. Feinschliff inklusive.",
+      text: "Von Blättern und Mundstücken bis hin zu Gurten und Pflegemitteln – finde bei uns hochwertiges Zubehör, um das Beste aus deinem Saxofon herauszuholen",
+			link: "/shop",
+      linkText: "Jetzt stöbern",
+      imageUrl: "",
+    },
+    {
+      title: "Saitenheld gesucht? ",
+      text: "Entdecke eine breite Palette an Saiten, Plektren, Verstärkern, Effekten und Cases, um deinen Gitarrensound zu individualisieren und dein Equipment optimal auszustatten",
+      link: "/shop",
+      linkText: "Jetzt Zubehör entdecken",
+      imageUrl: "",
+    },
+    {
+      title: "Melodien zum Leben erwecken.",
+      text: "Finde bei uns Stative, Bänke, Taschen, Kabel und mehr, um dein Keyboard-Spiel komfortabler zu gestalten und dein Equipment sicher zu transportieren.",
+      link: "/shop",
+      linkText: "Jetzt Setup weitern",
+      imageUrl: "",
+    },
+  ]
