d54ee9240b91f63679c4f31fdc3d1670c4327160
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-05-19 14:52:14 +0200

    line removal

--- src/routes/ProductDetailPage.jsx
+++ src/routes/ProductDetailPage.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react"
 import { useLoaderData, useParams } from "react-router-dom"
-import AddToCart from "../components/shop/AddToCart.jsx"
-import AmountInput from "../components/shop/AmountInput.jsx"
 import UserInput from '../components/shop/UserInput.jsx'
 
 export default function ProductDetailPage() {
