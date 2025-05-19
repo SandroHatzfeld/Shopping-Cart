9b93f1d2dc083d3a3cd822880cc48dcaee481971
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-05-19 12:14:09 +0200

    setting up tests

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ react() ],
+  plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setup.js',
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
   },
-})
+});
