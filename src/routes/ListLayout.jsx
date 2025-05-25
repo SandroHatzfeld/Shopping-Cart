0408163139c99b3e373feed842326dc868746351
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-05-25 13:27:44 +0200

    start responsivity

--- src/routes/ListLayout.jsx
+++ src/routes/ListLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet, useLoaderData } from "react-router-dom"
 import CategoryListItem from '../components/shop/CategoryListItem.jsx'
+import { useState } from 'react'
 
 export default function ListLayout() {
   const data = useLoaderData()
 
+  const [navCollapsed, setNavCollapsed] = useState(true)
+
   return (
     <>
       <main id="list-layout">
-        <aside>
+        <div className={`toggleSidebar ${navCollapsed && 'collapsed'}`} onClick={() => setNavCollapsed(!navCollapsed)}></div>
+        <aside className={navCollapsed && 'collapsed'}>
           {data.categories.map((category) => {            
             return (
               <CategoryListItem level={0} key={category.name} name={category.name} subcategories={category.subcategories}/>
