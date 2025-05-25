19a898eec1da22734357233c322cb078c5eac648
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-05-25 11:29:37 +0200

    collapsing function

--- src/components/shop/CategoryListItem.jsx
+++ src/components/shop/CategoryListItem.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
 
 export default function CategoryListItem({ level, name, subcategories }) {
-  
+  const [isCollapsed, setIsCollapsed] = useState(true)
+
+  const handleToggleList = () => {
+    setIsCollapsed(!isCollapsed)
+  }
+
   return (
-    <div className={`category-item category-level-${level}`}>
+    <div
+      className={`category-item category-level-${level} ${
+        isCollapsed && "collapsed"
+      }`}
+    >
       <div className="category-title">
-        <NavLink to={name}>{name}</NavLink>
-        <button className="toggleListing"></button>
+        <NavLink
+          to={name}
+        >
+          {name}
+        </NavLink>
+        {subcategories && (
+          <button onClick={handleToggleList} className="toggleListing"></button>
+        )}
       </div>
       {subcategories ? (
         subcategories.map((category) => {
