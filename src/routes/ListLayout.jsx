f7c8527105f7864e131fe52ba7efb90f2bfec475
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-05-26 12:24:34 +0200

    added all products link in nav

--- src/routes/ListLayout.jsx
+++ src/routes/ListLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet, useLoaderData } from "react-router-dom"
-import CategoryListItem from '../components/shop/CategoryListItem.jsx'
-import { useState } from 'react'
-
+import CategoryListItem from "../components/shop/CategoryListItem.jsx"
+import { useState } from "react"
+import { NavLink } from 'react-router-dom'
 export default function ListLayout() {
   const data = useLoaderData()
 
@@ -10,11 +10,26 @@ export default function ListLayout() {
   return (
     <>
       <main id="list-layout">
-        <div className={`toggleSidebar ${navCollapsed && 'collapsed'}`} onClick={() => setNavCollapsed(!navCollapsed)}></div>
-        <aside className={navCollapsed && 'collapsed'}>
-          {data.categories.map((category) => {            
+        <div
+          className={`toggleSidebar ${navCollapsed && "collapsed"}`}
+          onClick={() => setNavCollapsed(!navCollapsed)}
+        ></div>
+        <aside className={navCollapsed && "collapsed"}>
+          <div className="category-item category-level-0">
+            <div className="category-title">
+              <NavLink to={"/shop"} end>
+                Alle Produkte
+              </NavLink>
+            </div>
+          </div>
+          {data.categories.map((category) => {
             return (
-              <CategoryListItem level={0} key={category.name} name={category.name} subcategories={category.subcategories}/>
+              <CategoryListItem
+                level={0}
+                key={category.name}
+                name={category.name}
+                subcategories={category.subcategories}
+              />
             )
           })}
         </aside>
